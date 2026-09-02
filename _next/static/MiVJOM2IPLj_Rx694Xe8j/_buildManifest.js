fea1eb9b9ad4dbaa2bcaa99487ce497931661e72
reddyprasadkv-sunsolv <reddyprasadkv@sunsolv.in>
2026-09-02 11:49:32 +0530

    deploy: static build to GitHub Pages

---
+++ _next/static/MiVJOM2IPLj_Rx694Xe8j/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sampathrani//_next/:path+",
+        "destination": "/sampathrani/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
